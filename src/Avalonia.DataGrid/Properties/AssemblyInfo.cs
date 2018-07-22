88556d2d30f9bb33fef3831922dc7110f86f0bef
Dan Walmsley <dan@walms.co.uk>
2018-07-22 11:11:45 +0100

    update avalonia.

--- src/Avalonia.DataGrid/Properties/AssemblyInfo.cs
+++ src/Avalonia.DataGrid/Properties/AssemblyInfo.cs
@@ -4,5 +4,3 @@ using Avalonia.Metadata;
 
 [assembly: XmlnsDefinition("https://github.com/avaloniaui", "Avalonia.Controls")]
 [assembly: XmlnsDefinition("https://github.com/avaloniaui", "Avalonia.Controls.Primitives")]
-//Avalonia.Controls.Primitives
-//Avalonia.Controls.Primitives
